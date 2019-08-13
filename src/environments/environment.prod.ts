31b0cad56941244d72d52c9fed923c139fd23634
Rafael <rafaelhecr@gmail.com>
2019-08-13 15:57:50 -0500

    Claves de autenticacion en Enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBYEYfsNWlUncHw_Mo8tNJu8nPDKGdxTrM",
+    authDomain: "sion-devcat-firebase.firebaseapp.com",
+    databaseURL: "https://sion-devcat-firebase.firebaseio.com",
+    projectId: "sion-devcat-firebase",
+    storageBucket: "sion-devcat-firebase.appspot.com",
+    messagingSenderId: "53047941804",
+    appId: "1:53047941804:web:a90091371ef498ba"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+      apiKey: "AIzaSyBYEYfsNWlUncHw_Mo8tNJu8nPDKGdxTrM",
+      authDomain: "sion-devcat-firebase.firebaseapp.com",
+      databaseURL: "https://sion-devcat-firebase.firebaseio.com",
+      projectId: "sion-devcat-firebase",
+      storageBucket: "sion-devcat-firebase.appspot.com",
+      messagingSenderId: "53047941804",
+      appId: "1:53047941804:web:a90091371ef498ba"
+  }
 };
 
 /*
